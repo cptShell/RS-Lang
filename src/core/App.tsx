93f305fe0b640d489354c34ee9c9e76755b66637
Andrey Peshko <coursestud@mail.ru>
2022-02-04 11:09:43 +0300

    feat: add webpack, react and typescript

---
+++ src/core/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+export const App = () => {
+  useEffect(() => {
+  });
+  return (
+    <div>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
+};
+
