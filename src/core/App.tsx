11aea5562d4722c8e9c3109e3909546d52fccaa2
Andrey Peshko <coursestud@mail.ru>
2022-02-06 16:03:08 +0300

    feat: add footer

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import { listRoutes } from '../routes/routes';
 
@@ -13,6 +14,7 @@ export const App = () => {
       <div className='main-container'>
         <Routes>{listPages}</Routes>
       </div>
+      <Footer />
     </>
   );
 };
