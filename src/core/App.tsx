9d68c50afa816b48cdd7625b08d33e272bf0f810
Andrey Peshko <coursestud@mail.ru>
2022-02-06 15:59:26 +0300

    feat: add routes

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Routes, Route } from 'react-router-dom';
+import { listRoutes } from '../routes/routes';
 
 export const App = () => {
+  const listPages = listRoutes.map((routeData) => {
+    return <Route key={routeData.route} path={routeData.route} element={routeData.component} />;
+  });
   return (
     <>
       <Header />
+      <div className='main-container'>
+        <Routes>{listPages}</Routes>
+      </div>
     </>
   );
 };
