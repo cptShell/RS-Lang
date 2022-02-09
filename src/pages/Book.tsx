25387fbd5b649d2b1c50f13236854e06448c6fed
cptShell <repozheka@gmail.com>
2022-02-09 16:37:59 +0300

    refactor: userstate getter

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { BASE_APP_URL, MESSAGE_IS_AUTH } from '../utils/constants/constants';
+import { MESSAGE_IS_AUTH } from '../utils/constants/constants';
 import { BookPage } from './wordbook/bookPage';
 import { BookPagination } from './wordbook/bookPagination';
 import { GroupList } from './wordbook/groupList';
+import { store } from '../redux/store';
 
 const Book = (): JSX.Element => {
   const [pageState, setPageState] = useState({page: 0, group: 0});
-  const storageData = JSON.parse(localStorage.getItem('rs-lang_RSS_22_Q3-STORAGE_ID-277') as string);
-  const isAuthorized: boolean = storageData.message === MESSAGE_IS_AUTH;
-  
+  const isAuthorized: boolean = store.getState().userData.message === MESSAGE_IS_AUTH;
+
   return ( 
     <div className='container-fluid d-flex flex-column gap-2 p-2'>
       <BookPagination pageState={pageState} setPageState={setPageState}/>
