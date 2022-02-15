a0b5b5de930c5e5ea41d81ab82ad2baced470cbf
Andrey Peshko <coursestud@mail.ru>
2022-02-15 18:30:05 +0300

    feat: add audio buttons

--- src/components/audiocall/AudiocallGame.tsx
+++ src/components/audiocall/AudiocallGame.tsx
@@ -2,14 +2,25 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nextQuestion, resetGame } from '../../redux/actions/audiocallCreator';
 import { RootState } from '../../redux/store';
+import { BASE_APP_URL } from '../../utils/constants/constants';
+import useAudio from '../../utils/hooks/useAudio';
+import AudioButton from '../sprint/AudioButton';
 
 const Audiocall = () => {
+  const [playAudioRight] = useAudio('./sounds/right.mp3');
+  const [playAudioFail] = useAudio('./sounds/fail.mp3');
   const {
     audiocall: { counter, listQuestions },
   } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
 
-  const onCheckAnswer = () => {
+  const onCheckAnswer = (isAnswerRight: boolean) => {
+    if (isAnswerRight) {
+      playAudioRight();
+    } else {
+      playAudioFail();
+    }
+    
     if (counter < listQuestions.length - 1) {
       dispatch(nextQuestion(counter + 1));
     } else {
@@ -23,16 +34,16 @@ const Audiocall = () => {
     };
   },[]);
 
-  const listAnswers = listQuestions[counter].wordsAnswers.map((word, index) => (
+  const listAnswers = listQuestions[counter].wordsAnswers.map((wordData, index) => (
     <li key={index}>
-      <button onClick={onCheckAnswer} data-number={index + 1}>{`${index + 1} ${word.wordTranslate}`}</button>
+      <button onClick={onCheckAnswer.bind(null, wordData.isRight)} data-number={index + 1}>{`${index + 1} ${wordData.wordTranslate}`}</button>
     </li>
   ));
 
   return (
     <>
       <h1>Audiocall game</h1>
-      <button>audio</button>
+      <AudioButton url={`${BASE_APP_URL}/${listQuestions[counter].audio}`}/>
       <ul>{listAnswers}</ul>
     </>
   );
