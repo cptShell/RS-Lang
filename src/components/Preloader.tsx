d274a0c4103ef3588341cf1da18f43ebe232a261
Andrey Peshko <coursestud@mail.ru>
2022-02-16 22:49:54 +0300

    refactor: fix shift rows

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Preloader = () => {
   return (
     <div className='preloader'>
-      <div className='spinner-border text-primary' role='status'>
-        {/* <span className='sr-only'>Loading...</span> */}
-      </div>
+      <div className='spinner-border text-primary' role='status'></div>
     </div>
   );
 };
